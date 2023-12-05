58fee3fa1e35d8ddc1fea923125e7628dcb5c60e
brucedev <whdtmek@gmail.com>
2023-12-05 18:34:48 +0900

    HomePage UI 렌더링

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,8 +1,8 @@
+import Search from "./components/sections/Search";
+
 export default function HomePage() {
     return (
-        <div className="container mx-auto">
-            <h1 className="text-4xl font-bold text-center">Home Page</h1>
-            <p className="text-center">This is the home page</p>
-        </div>
-    )
+        <div className="flex flex-col px-3 space-y-5">
+            <Search/>
+        </div>)
 }
